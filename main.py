935f9351417de60bab833a73c5e0c588404839e9
trakan <jakubbirn@gmail.com>
2022-10-30 22:27:22 +0100

    first try

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 
 
 if __name__=="__main__":
-    print("Hello world")
-    print("Hello world")
-    print("Hello world")
-    print("Hello world")
-    print("Hello world")
+    import bluetooth
+    print("\n Scanning for bluetooth devices:")
+    
+    devices = bluetooth.discover_devices(lookup_names = True, lookup_class = True)
+    
+    number_of_devices = len(devices)
+    print(number_of_devices,"devices found")
+    for addr, name, device_class in devices:
+        print("\n Device:")
+        print("Device Name: %s" % (name))
+        print("Device Mac Address: %s" %(addr))
+        print("\n")
+        print("Device Class: %s" % (device_class))
+        print("\n")
+        
+          
     
     
     

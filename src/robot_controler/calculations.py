414b3800526e4fe5323ef0e01d303451274a9fea
Trakan-ctrl <jakubbirn@gmail.com>
2023-01-30 10:38:35 +0100

    'servo_changes_30_7'

--- src/robot_controler/calculations.py
+++ src/robot_controler/calculations.py
@@ -14,7 +14,7 @@ def angle_to_pwm(angle, server_version: int):
         elif server_version == 3:
                 dc = 3.5+float(angle)*(10/180)
         elif server_version == 4:
-                dc = 5+float(angle)*(5/180)
+                dc = 2+float(angle)*(10/180)
 
         else:
                 print("Specified servo doesn't exist!!!")

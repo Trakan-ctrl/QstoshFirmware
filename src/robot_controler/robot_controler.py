5123c08283d04ad875c8930e7c0ec38ba1cc5943
Trakan-ctrl <jakubbirn@gmail.com>
2023-02-04 13:29:04 +0100

    servo_4_10

--- src/robot_controler/robot_controler.py
+++ src/robot_controler/robot_controler.py
@@ -20,7 +20,7 @@ class RobotControler:
                             "position": {
                                 "base_vertical_rotation": 90.,
                                 "base_horizontal_rotation": 70.,
-                                "extension_rotation": 90.,
+                                "extension_rotation": 120.,
                                 "grabbed": 90.
                             }}
         self.servo_dictionary = {
